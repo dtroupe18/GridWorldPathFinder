b13adc26fceb4566e971e8cb63d87007c6c24ebc
Dave <Dave.Troupe@gmail.com>
2017-02-17 16:13:55 -0500

    A_Star_LowerG works Green = Start Red = Goal Blue = Explored cells Orange = Shortest Path

---
+++ A_Star_HigherG.py
@@ -0,0 +1,9 @@
+"""
+A* path-finding algorithm
+Manhattan distance is the heuristic
+Agent can only move in the four cardinal directions
+(No Diagonal moves) tie breaking favors cells with
+a lower g cost.
+
+"""
+
